ba3f2db098f78ecc2561345c0861448917453202
unknown <rambsong1995@gmail.com>
2022-10-20 23:11:16 +0700

    updated

--- server.js
+++ server.js
@@ -6,17 +6,37 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
     // set headers content type
     res.setHeader('Content-Type', 'text/html');
-    let path = './views';
-    
-    fs.readFile('./views/index.html', (err, data) => {
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            path += 'about.html';
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err);
             res.end();
         }else{
-            res.write(data);
-            res.end();
+            res.end(data);
         }
-    })
+    });
 });
 server.listen (3000, 'localhost', () =>{
    console.log('listening for requests on port 3000');
